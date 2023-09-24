540ed6fcc4d40fecfafee122f0d64aed98e7f239
abhos <alecbhoskins@gmail.com>
2023-09-24 16:02:04 -0500

    removed hardcoded data

--- recipe-sharing-client/src/views/recipes/RecipeDetails.vue
+++ recipe-sharing-client/src/views/recipes/RecipeDetails.vue
@@ -6,47 +6,7 @@
 import RecipeForm from '@/components/forms/RecipeForm.vue'
 
 //#region Select Lists
-const selectLists = {
-    ingredients: [
-        { text: "Cabbage", value: 5 },
-        { text: "Spinach", value: 3 },
-        { text: "Ham", value: 9 },
-        { text: "Pickles", value: 7 },
-        { text: "Mustard Seeds", value: 1 }, { text: "Cabbage", value: 5 },
-        { text: "Spinach", value: 3 },
-        { text: "Ham", value: 9 },
-        { text: "Pickles", value: 7 },
-        { text: "Mustard Seeds", value: 1 }, { text: "Cabbage", value: 5 },
-        { text: "Spinach", value: 3 },
-        { text: "Ham", value: 9 },
-        { text: "Pickles", value: 7 },
-        { text: "Mustard Seeds", value: 1 }, { text: "Cabbage", value: 5 },
-        { text: "Spinach", value: 3 },
-        { text: "Ham", value: 9 },
-        { text: "Pickles", value: 7 },
-        { text: "Mustard Seeds", value: 1 }, { text: "Cabbage", value: 5 },
-        { text: "Spinach", value: 3 },
-        { text: "Ham", value: 9 },
-        { text: "Pickles", value: 7 },
-        { text: "Mustard Seeds", value: 1 }, { text: "Cabbage", value: 5 },
-        { text: "Spinach", value: 3 },
-        { text: "Ham", value: 9 },
-        { text: "Pickles", value: 7 },
-        { text: "Mustard Seeds", value: 1 }, { text: "Cabbage", value: 5 },
-        { text: "Spinach", value: 3 },
-        { text: "Ham", value: 9 },
-        { text: "Pickles", value: 7 },
-        { text: "Mustard Seeds", value: 1 }, { text: "Cabbage", value: 5 },
-        { text: "Spinach", value: 3 },
-        { text: "Ham", value: 9 },
-        { text: "Pickles", value: 7 },
-        { text: "Mustard Seeds", value: 1 }, { text: "Cabbage", value: 5 },
-        { text: "Spinach", value: 3 },
-        { text: "Ham", value: 9 },
-        { text: "Pickles", value: 7 },
-        { text: "Mustard Seeds", value: 1 },
-    ]
-};
+
 //#endregion
 
 </script>
